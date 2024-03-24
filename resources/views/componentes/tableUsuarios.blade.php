0ddfb96e4c74496fbdbcff11e172700b15fdd114
saracollados <saracollados95@gmail.com>
2024-03-24 23:38:04 +0000

    cambios en la tabla de usuarios para añadir el botón de editar un usuario

--- resources/views/componentes/tableUsuarios.blade.php
+++ resources/views/componentes/tableUsuarios.blade.php
@@ -5,13 +5,20 @@
     <td class="px-6 py-3 text-center">{{$item->fecha_nacimiento}}</td>
     <td class="px-6 py-3 text-center">{{$item->genero}}</td>
     <td class="px-6 py-3 text-center">{{$item->email}}</td>
-    <td class="px-6 py-3 text-center">
-        <a class="py-2.5 text-sm my-0 mx-0.5 px-4 rounded-lg text-dark-blue" href="/editarUsuario/{{$item->id}}" title="Editar Usuario">
-            <i class="fa-solid fa-pencil"></i>
-        </a>
-        <button class="py-2 text-sm my-0 mx-0.5 px-3 rounded-lg text-dark-blue eliminar-usuario" title="Eliminar Usuario" data-id={{$item->id}}>
+
+    <td class="px-6 py-3 flex flex-row justify-center">
+        <form action="{{route('editarPerfilUsuario')}}" method="POST">
+            @csrf
+            <input type="hidden" name="id" value="{{$item->id}}">
+
+            <button type="submit" class="py-2.5 text-sm my-0 mx-0.5 px-4 rounded-lg text-dark-blue">
+                <i class="fa-solid fa-pencil"></i>
+            </button>
+        </form>
+        <button class="py-2 text-sm my-0 mx-0.5 px-3 rounded-lg text-dark-blue eliminar-usuario" title="Eliminar Usuario" data-id={{$item->id}} data-page='usuarios'>
             <i class="fa-solid fa-trash"></i>
         </button>
     </td>
+
 </tr>
 
